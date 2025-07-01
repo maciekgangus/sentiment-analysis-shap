fdc0be870af276d50bf142ccebadb1c3e125655a
Maciej Leśniak <maciek_gangus@MacBook-Air.local>
2025-07-01 23:40:30 +0200

    almost final version

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,11 +1,25 @@
 # utils/preprocessing.py
 import re
+from collections import defaultdict, Counter
 import pandas as pd
 import torch
 import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import shap
+import numpy as np
+import matplotlib.pyplot as plt
 
+DEFAULT_STOPWORDS = set("""
+i me my myself we our ours ourselves you your yours yourself yourselves
+he him his himself she her hers herself it its itself they them their theirs
+themselves what which who whom this that these those am is are was were be
+been being have has had having do does did doing a an the and but if or because
+as until while of at by for with about against between into through during before
+after above below to from up down in out on off over under again further then
+once here there when where why how all any both each few more most other some
+such no nor not only own same so than too very s t can will just don should now
+""".split())
 
 def combine_text(row):
     content = row['content'] if pd.notna(row['content']) else ''
@@ -13,12 +27,18 @@ def combine_text(row):
     mentions = row['mentions'] if pd.notna(row['mentions']) else ''
     return f"{content} {hashtags} {mentions}"
 
+
+
 def clean_tweet_for_vader(text: str) -> str:
-    text = re.sub(r"http\S+|www\.\S+", "", text)  # usuwa linki
-    text = re.sub(r"[@#]", "", text)  # usuwa tylko znaki @ i #, nie usuwa treści
-    text = re.sub(r"[\"\']", "", text)  # usuwa tylko znaki " i '
+    text = re.sub(r"pic\.twitter\.com/\S+", "<IMG>", text)
+    text = re.sub(r"http\S+|www\.\S+", "", text)
+    text = re.sub(r"[@#]", "", text)
+    text = re.sub(r"[\"\']", "", text)
     text = re.sub(r"\s+", " ", text).strip()
+    text = re.sub(r"[^\w\s]", "", text)
+    text = ' '.join([word for word in text.split() if word.lower() not in DEFAULT_STOPWORDS])
     return text
+
 
 def classify_sentiment(score: float) -> str:
     if score >= 0.05:
@@ -28,23 +48,20 @@ def classify_sentiment(score: float) -> str:
     else:
         return "neutral"
 
+
 def sentiment_to_number(label: str) -> int:
     mapping = {'neutral': 0, 'positive': 1, 'negative': 2}
     return mapping.get(label, -1)
 
+
 def preprocess_tweet_bert(text: str) -> str:
     text = re.sub(r"http\S+", "http", text)
     text = re.sub(r"pic\.twitter\.com/\S+", "<IMG>", text)
-    text = re.sub(r"@\S+", "@user", text)
+    text = re.sub(r"[@#]", "", text)
+    text = re.sub(r"[\"\']", "", text)
     text = re.sub(r"\s+", " ", text).strip()
     return text
 
-def preprocess_simple_tokenize(text: str) -> str:
-    text = text.lower()
-    text = re.sub(r"http\S+", "<URL>", text)
-    text = re.sub(r"[^a-z0-9\s]", "", text)  # usuwa znaki specjalne
-    text = re.sub(r"\s+", " ", text).strip()
-    return text
 
 def run_vader_analysis(df: pd.DataFrame) -> pd.DataFrame:
     analyzer = SentimentIntensityAnalyzer()
@@ -55,6 +72,7 @@ def run_vader_analysis(df: pd.DataFrame) -> pd.DataFrame:
     df["vader_sentiment_label"] = df["compound"].apply(classify_sentiment)
     df["vader_numeric"] = df["vader_sentiment_label"].map(sentiment_to_number)
     return df[["vader_clean", "vader_sentiment_label", "vader_numeric"]]
+
 
 def run_bert_analysis(df: pd.DataFrame,
                       model_name="cardiffnlp/twitter-roberta-base-sentiment-latest",
@@ -90,3 +108,5 @@ def run_bert_analysis(df: pd.DataFrame,
     df["bert_numeric"] = df["bert_sentiment_label"].map(sentiment_to_number)
     df["bert_probs"] = all_probs
     return df[["bert_clean", "bert_sentiment_label", "bert_numeric", "bert_probs"]]
+
+
